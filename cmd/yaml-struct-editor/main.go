a95f0df5515134c73c6f93efd5abe894e3da8ac3
yosuke moriyama <`>
2026-06-04 21:46:53 +0900

    chore: 統合動作を確認

--- cmd/yaml-struct-editor/main.go
+++ cmd/yaml-struct-editor/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	appservice "github.com/mryskyj/yaml-editor/app"
 	"github.com/wailsapp/wails/v3/pkg/application"
@@ -14,6 +15,9 @@ func main() {
 		Description: "YAML editor powered by Go struct schemas",
 		Services: []application.Service{
 			application.NewService(service),
+		},
+		Assets: application.AssetOptions{
+			Handler: application.AssetFileServerFS(os.DirFS("frontend/dist")),
 		},
 		Mac: application.MacOptions{
 			ApplicationShouldTerminateAfterLastWindowClosed: true,
